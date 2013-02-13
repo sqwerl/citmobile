be250ecbcdda5af74265a41b04ca232b2eb1dc9a
Larry Cao <larrycao@berkeley.edu>
2013-02-12 23:14:23 -0800

    Initial Commit

---
+++ CreateItTests/CreateItTests.h
@@ -0,0 +1,13 @@
+//
+//  CreateItTests.h
+//  CreateItTests
+//
+//  Created by Larry Cao on 2/12/13.
+//  Copyright (c) 2013 Larry Cao. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CreateItTests : SenTestCase
+
+@end
---
+++ CreateItTests/CreateItTests.m
@@ -0,0 +1,32 @@
+//
+//  CreateItTests.m
+//  CreateItTests
+//
+//  Created by Larry Cao on 2/12/13.
+//  Copyright (c) 2013 Larry Cao. All rights reserved.
+//
+
+#import "CreateItTests.h"
+
+@implementation CreateItTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in CreateItTests");
+}
+
+@end
